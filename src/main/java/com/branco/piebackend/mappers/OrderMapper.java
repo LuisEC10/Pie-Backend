a1315e35e37b3d6554f1e0850fb82fb5edd129c8
LuisEC10 <piedpiperec10@gmail.com>
2026-01-06 01:40:52 -0500

    feat(order): little change in OrderMapper so the Response is completed)

--- src/main/java/com/branco/piebackend/mappers/OrderMapper.java
+++ src/main/java/com/branco/piebackend/mappers/OrderMapper.java
@@ -1,6 +1,8 @@
 package com.branco.piebackend.mappers;
 
 import com.branco.piebackend.entities.OrderEntity;
+import com.branco.piebackend.entities.OrderItemEntity;
+import com.branco.piebackend.models.order.OrderItemResponseDTO;
 import com.branco.piebackend.models.order.OrderResponseDTO;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,23 @@ public class OrderMapper {
                 .state(entity.getState())
                 .sellerId(entity.getSeller().getId())
                 .brand(entity.getSeller().getBrand())
+                .shipLatitude(entity.getShipLatitude())
+                .shipLongitude(entity.getShipLongitude())
+                .items(entity.getOrderItems().stream()
+                        .map(this::convertItemToDTO)
+                        .toList())
+                .build();
+    }
+
+    private OrderItemResponseDTO convertItemToDTO(OrderItemEntity itemEntity) {
+        return OrderItemResponseDTO.builder()
+                .id(itemEntity.getId())
+                .productId(itemEntity.getProduct().getId())
+                .productName(itemEntity.getProduct().getName())
+                .productImageURL(itemEntity.getProduct().getImageURL())
+                .quantity(itemEntity.getQuantity())
+                .priceAtPurchase(itemEntity.getPriceAtPurchase())
+                .subtotal(itemEntity.getTotal())
                 .build();
     }
 }
